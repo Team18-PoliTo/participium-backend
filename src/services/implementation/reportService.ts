261c090fd128c31d73861cd1c1d1680257c24bf8
JonKurmaku <jonikurmaku@gmail.com>
2025-11-11 23:16:30 +0100

    bug-fix

--- src/services/implementation/reportService.ts
+++ src/services/implementation/reportService.ts
@@ -6,6 +6,7 @@ import { ReportRepository } from "../../repositories/implementation/ReportReposi
 import CitizenRepository from "../../repositories/implementation/CitizenRepository";
 import { ICitizenRepository } from "../../repositories/ICitizenRepository";
 import MinIoService from "../MinIoService";
+import { v4 as uuidv4 } from "uuid";
 class ReportService {
   constructor(
     private reportRepository: IReportRepository = new ReportRepository(),
@@ -38,27 +39,38 @@ class ReportService {
       newReport.photo3 = pathPrefix + data.binaryPhoto3.filename;
     }
       
-    const uploadedPhotos: string[] = [];
+    const uploadedPhotos: { [key: string]: string } = {};
 
-    const photos = [
-      data.binaryPhoto1,
-      data.binaryPhoto2,
-      data.binaryPhoto3,
-    ].filter((p): p is NonNullable<typeof p> => !!p); // remove undefined
-
-    for (const photo of photos) {
-      const objectKey = pathPrefix + photo.filename;
-
-      // Upload file binary data to MinIO
-      await MinIoService.uploadFile(objectKey, photo.data, photo.mimetype);
-
-      uploadedPhotos.push(objectKey);
+    if (data.binaryPhoto1) {
+      const objectKey1 = `${pathPrefix}${uuidv4()}-${data.binaryPhoto1.filename}`;
+      uploadedPhotos.photo1 = await MinIoService.uploadFile(
+        objectKey1,
+        data.binaryPhoto1.data,
+        data.binaryPhoto1.mimetype
+      );
     }
 
-    // Assign uploaded MinIO object keys to the report entity
-    if (uploadedPhotos[0]) newReport.photo1 = uploadedPhotos[0];
-    if (uploadedPhotos[1]) newReport.photo2 = uploadedPhotos[1];
-    if (uploadedPhotos[2]) newReport.photo3 = uploadedPhotos[2];
+    if (data.binaryPhoto2) {
+      const objectKey2 = `${pathPrefix}${uuidv4()}-${data.binaryPhoto2.filename}`;
+      uploadedPhotos.photo2 = await MinIoService.uploadFile(
+        objectKey2,
+        data.binaryPhoto2.data,
+        data.binaryPhoto2.mimetype
+      );
+    }
+
+    if (data.binaryPhoto3) {
+      const objectKey3 = `${pathPrefix}${uuidv4()}-${data.binaryPhoto3.filename}`;
+      uploadedPhotos.photo3 = await MinIoService.uploadFile(
+        objectKey3,
+        data.binaryPhoto3.data,
+        data.binaryPhoto3.mimetype
+      );
+    }
+
+    if (uploadedPhotos.photo1) newReport.photo1 = uploadedPhotos.photo1;
+    if (uploadedPhotos.photo2) newReport.photo2 = uploadedPhotos.photo2;
+    if (uploadedPhotos.photo3) newReport.photo3 = uploadedPhotos.photo3;
 
     await this.reportRepository.update(newReport);
 
